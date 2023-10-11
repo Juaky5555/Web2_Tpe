1db96376b16bd12d042ec5e3c5c2cdeab6c1986e
Joaquin Magiorano <joacopirovano@gmail.com>
2023-10-11 16:52:12 -0300

    se actualizo el router

--- router.php
+++ router.php
@@ -1,9 +1,9 @@
 <?php
-require_once 'app/individuos.php';
+require_once './app/controllers/individuos.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'mostrar';
+$action = 'listar';
 if (!empty( $_GET['action'])) {
     $action = $_GET['action'];
 }
@@ -17,23 +17,29 @@ if (!empty( $_GET['action'])) {
 $params = explode('/', $action);
 
 switch ($params[0]) {
-    case 'mostrar':
-        mostrarIndividuos();
+    case 'listar':
+        $controller = new controladorIndividuos();
+        $controller-> mostrarIndividuos_control();
         break;
     case 'agregar':
-        añadirIndividuo();
+        $controller = new controladorIndividuos();
+        $controller-> sumarIndividuo();
         break;
     case 'eliminar':
-        eliminarIndividuo($params[1]);
+        $controller = new controladorIndividuos();
+        $controller-> eliminarIndividuo($params[1]);
         break;
-    case 'individuo':
-        mostrarIndividuoEnDetalle($params[1]);
+    case 'mostrar':
+        $controller = new controladorIndividuos();
+        $controller-> mostrarIndividuoEnDetalle_control($params[1]);
         break;
     case 'mostrarParaModificar':
-        mostrarIndividuoModificar($params[1]);
+        $controller = new controladorIndividuos();
+        $controller-> mostrarIndividuoAModificar_control($params[1]);
         break;
     case 'modificar':
-        modificarDatos();
+        $controller = new controladorIndividuos();
+        $controller-> modificarDatos();
         break;
     default: 
         echo "404 Page Not Found";
